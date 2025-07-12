e2167fbf41616d6a9f15d3b24a993bd448eef214
loulou1945 <smirnova_vika2014@mail.ru>
2025-07-12 20:33:57 +0300

    Initial commit

---
+++ school/src/test/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,42 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("student")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("{id}")
+    public Student getStudent(@PathVariable Long id) {
+        return studentService.findStudent(id);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+
+    @PutMapping
+    public Student editStudent(@RequestBody Student student) {
+        return studentService.editStudent(student);
+    }
+
+    @DeleteMapping("{id}")
+    public Student deleteStudent(@PathVariable Long id) {
+        return studentService.deleteStudent(id);
+    }
+
+    @GetMapping("{age}")
+    public Collection<Student> findStudentByAge(@PathVariable int age) {
+        return studentService.findStudentByAge(age);
+    }
+}
